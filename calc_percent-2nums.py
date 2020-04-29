8c6086fbc9d891d5e7d9f19461a495d0b1a1e0c3
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-29 15:32:13 -0400

    Update calc_percent-2nums.py

--- calc_percent-2nums.py
+++ calc_percent-2nums.py
@@ -1,6 +1,5 @@
 # https://stackoverflow.com/questions/5997987/is-there-an-operator-to-calculate-percentage-in-python
 print("Calculates what percentage one number is of another.")
-#print("Then it takes the result and and 
 
 daPercentof = float(input("Enter the percentage number: "))
 daNumba = float(input("Enter the main number: "))
